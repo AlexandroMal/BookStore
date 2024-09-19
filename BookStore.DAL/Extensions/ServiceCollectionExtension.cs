dc0c06fcc9eb279a6778c0576fceac5172eaa842
AlexandroMal <Sashamalina8695@gmail.com>
2024-09-19 16:58:49 +0300

    create service sollection extension

---
+++ BookStore.DAL/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.DAL.Extensions
+{
+    public static class ServiceCollectionExtension
+    {
+        public static void InjectDatabaseServices(this IServiceCollection services)
+        {
+            services.AddDatabase();
+        }
+
+        public static void AddDatabase(this IServiceCollection services)
+        {
+            //add database here
+        }
+    }
+}
