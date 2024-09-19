50452a782d97a1e48a259c889a7b2fb0a5bc5084
AlexandroMal <Sashamalina8695@gmail.com>
2024-09-19 16:55:39 +0300

    add datavase context in project

---
+++ BookStore.DAL/Context/QuizHubDatabaseContext.cs
@@ -0,0 +1,24 @@
+﻿using BookStore.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.DAL.Context
+{
+    public class QuizHubDatabaseContext : DbContext
+    {
+        public QuizHubDatabaseContext() { }
+
+        public QuizHubDatabaseContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
