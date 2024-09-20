28cb293d115c31568e9ca2c96f016600b2d0ff4a
AlexandroMal <Sashamalina8695@gmail.com>
2024-09-20 16:34:42 +0300

    create user configuration

---
+++ BookStore.DAL/EntityConfiguration/UserConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using BookStore.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.DAL.EntityConfiguration
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(50);
+            builder.Property(x => x.Surname).IsRequired().HasMaxLength(100);
+            builder.Property(x => x.Password).IsRequired().HasMaxLength(255);
+            builder.Property(x => x.PhoneNumber).IsRequired().HasMaxLength(20);
+
+        }
+    }
+}
