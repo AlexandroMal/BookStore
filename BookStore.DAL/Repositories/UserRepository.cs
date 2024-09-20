ff60580850677dd9828388d5ba10092e288d3a23
AlexandroMal <Sashamalina8695@gmail.com>
2024-09-20 16:33:43 +0300

    Added base repository for user entity

---
+++ BookStore.DAL/Repositories/UserRepository.cs
@@ -0,0 +1,23 @@
+﻿using BookStore.DAL.Context;
+using BookStore.DAL.Entities;
+using BookStore.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.DAL.Repositories
+{
+    public class UserRepository : BaseRepository<User>, IUserRepository
+    {
+        public UserRepository(QuizHubDatabaseContext dbContext) : base(dbContext)
+        {
+        }
+
+        public Task<User> GetUserByCredentialsAsync(string name, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
