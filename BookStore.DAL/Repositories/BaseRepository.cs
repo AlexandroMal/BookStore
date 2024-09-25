9fc2edf33441f24dfd4b3b98afc62ed841a15a97
AlexandroMal <Sashamalina8695@gmail.com>
2024-09-25 17:33:51 +0300

    make correct name DbContext and implemented methods

--- BookStore.DAL/Repositories/BaseRepository.cs
+++ BookStore.DAL/Repositories/BaseRepository.cs
@@ -3,6 +3,7 @@ using BookStore.DAL.Entities;
 using BookStore.DAL.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,36 +13,42 @@ namespace BookStore.DAL.Repositories
     public abstract class BaseRepository<TEntity> : IBaseRepository<TEntity>
         where TEntity : BaseEntity
     {
-        protected QuizHubDatabaseContext _dbContext;
+        protected AppDatabaseContext _dbContext;
 
-        protected BaseRepository(QuizHubDatabaseContext dbContext)
+        protected BaseRepository(AppDatabaseContext dbContext)
         {
             _dbContext = dbContext;
         }
 
-        public Task<TEntity?> GetByIdAsync(Guid Id)
+        public async Task<TEntity?> GetByIdAsync(Guid Id)
         {
-            throw new NotImplementedException();
+            var result = await _dbContext.Set<TEntity>().FindAsync(Id);
+            return result;
         }
 
-        public Task<IEnumerable<TEntity>> GetAllAsync() 
-        {  
-            throw new NotImplementedException(); 
+        public async Task<IEnumerable<TEntity>> GetAllAsync() 
+        {
+            var result = await _dbContext.Set<TEntity>().ToListAsync();
+            return result;
         }
 
-        public Task<TEntity> AddAsync(TEntity entity)
+        public async Task AddAsync(TEntity entity)
         {
-            throw new NotImplementedException();
+            await _dbContext.Set<TEntity>().AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
         }
 
-        public Task<TEntity> UpdateAsync(TEntity entity)
+        public async Task UpdateAsync(TEntity entity)
         {
-            throw new NotImplementedException();
+            _dbContext.Set<TEntity>().Update(entity);
+            await _dbContext.SaveChangesAsync();
         }
 
-        public Task<TEntity> DeleteAsync(Guid id)
+        public async Task DeleteAsync(Guid id)
         {
-            throw new NotImplementedException();
+            var result = await _dbContext.Set<TEntity>().FirstOrDefaultAsync(x => x.Id == id);
+            _dbContext.Set<TEntity>().Remove(result);
+            await _dbContext.SaveChangesAsync();
         }
     }
 }
