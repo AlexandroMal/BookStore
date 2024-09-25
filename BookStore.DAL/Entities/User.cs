36ce8254569a81cd6c576f81cd0655b853473875
AlexandroMal <Sashamalina8695@gmail.com>
2024-09-25 17:41:23 +0300

    made username

--- BookStore.DAL/Entities/User.cs
+++ BookStore.DAL/Entities/User.cs
@@ -8,7 +8,8 @@ namespace BookStore.DAL.Entities
 {
     public class User : BaseEntity
     {
-        public string Name { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
         public string Surname { get; set; }
         public string Password { get; set; }
         public string PhoneNumber { get; set; }
