25c4849a1f534d92edf5f4dcf557c38440035256
AlexandroMal <Sashamalina8695@gmail.com>
2024-09-25 17:38:17 +0300

    made the correct name

---
+++ BookStore.DAL/Context/AppDatabaseContext.cs
@@ -0,0 +1,24 @@
+﻿using BookStore.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.DAL.Context
+{
+    public class AppDatabaseContext : DbContext
+    {
+        public AppDatabaseContext() { }
+
+        public AppDatabaseContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
