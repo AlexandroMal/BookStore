27c3e5ba09a3c4d164f6e82d7d9b462fea8c490e
AlexandroMal <Sashamalina8695@gmail.com>
2024-09-25 17:43:26 +0300

    added interfaces for services

---
+++ BookStore.BLL/Interfaces/IRegistrationService.cs
@@ -0,0 +1,14 @@
+﻿using BookStore.BLL.Models.AddModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.BLL.Interfaces
+{
+    public interface IRegistrationService
+    {
+        public Task RegisterAsync(AddUserModel addUser);
+    }
+}
---
+++ BookStore.BLL/Interfaces/IUserService.cs
@@ -0,0 +1,20 @@
+﻿using BookStore.BLL.Models;
+using BookStore.BLL.Models.AddModels;
+using BookStore.BLL.Models.UpdateModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.BLL.Interfaces
+{
+    public interface IUserService
+    {
+        public Task AddUserAsync(AddUserModel userModel);
+        public Task UpdateUserAsync(UpdateUserModel userModel);
+        public Task DeleteUserAsync(Guid id);
+        public Task<UserModel> GetUserAsync(Guid id);
+
+    }
+}
