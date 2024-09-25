9e98a1d2fc9b795cea2ce40f862b1612e7e0a821
AlexandroMal <Sashamalina8695@gmail.com>
2024-09-25 17:44:20 +0300

    added user models

---
+++ BookStore.BLL/Models/AddModels/AddUserModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.BLL.Models.AddModels
+{
+    public class AddUserModel
+    {
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public string Password { get; set; }
+
+    }
+}
---
+++ BookStore.BLL/Models/UserModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.BLL.Models
+{
+    public class UserModel
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+
+    }
+}
