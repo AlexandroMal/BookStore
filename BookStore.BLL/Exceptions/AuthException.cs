d40c9c427c2dcfd9f31764c95940f97bbae0610a
AlexandroMal <Sashamalina8695@gmail.com>
2024-09-25 17:42:57 +0300

    added exception

---
+++ BookStore.BLL/Exceptions/AuthException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.BLL.Exceptions
+{
+    public class AuthException : Exception
+    {
+        public AuthException() 
+            : base("Wrong login or password, authentication failed") { }
+    }
+}
---
+++ BookStore.BLL/Exceptions/ModelException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.BLL.Exceptions
+{
+    public class ModelIsEmptyException : Exception
+    {
+        public ModelIsEmptyException()
+            :base("Invalid model: one or several fields were empty") { }
+    }
+}
